375308ce09f073be32ae3503b6f3af349f3b473b
Daniel Guieb <35285976+danielaguieb@users.noreply.github.com>
2018-11-03 11:06:43 -0600

    Logic updates

--- Model.java
+++ Model.java
@@ -1,26 +1,36 @@
 import java.util.ArrayList;
 
 public class Model {
-	public ArrayList<Route> routes;
+	public Route[] routes;
 	
 	public Model() {
 		// construct routes
+		routes = new Route[3];
 		
 	}
 	
-	public int total_number_of_buses(int route_id) {
-		
-		return 0;
+	public int total_number_of_buses() {
+		int sum = 0;
+		for(int i=0; i<routes.length; i++) {
+//			sum += routes[i].get
+		}
+		return sum;
 	}
 	
-	public int total_cost(int route_id) {
-		
-		return 0;
+	public int total_cost() {
+		int sum = 0;
+		for(int i=0; i<routes.length; i++) {
+//			sum += routes[i].get
+		}
+		return sum;
 	}
 	
-	public int total_commute_time(int route_id) {
-		
-		return 0;
+	public int total_commute_time() {
+		int sum = 0;
+		for(int i=0; i<routes.length; i++) {
+//			sum += routes[i].get
+		}
+		return sum;
 	}
 	
 	// the algorithm
