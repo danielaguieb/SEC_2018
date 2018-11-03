7c5e3d18f5366553f57cb1dc2c5d44beff87308c
Shehroz <shehrozn1@gmail.com>
2018-11-03 13:27:27 -0600

    Update View.java

--- View.java
+++ View.java
@@ -3,6 +3,7 @@ import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,8 +15,9 @@ import javax.swing.*;
 public class View extends JFrame{
 	private Container container;
 	
-	JLabel total,BussesDeployed, Cost, CommuteTime, BusCostLabel, BusCapLabel;
-	JTextField BusCostEntry, BusCapEntry;
+	JLabel BussesDeployed, Cost, CommuteTime, BusCostLabel, BusCapLabel,Busses;
+	JTextField BusCostEntry, BusCapEntry, BusNum;
+	JTextArea Info;
 	JButton submit;
 	JComboBox route;
 	
@@ -37,10 +39,10 @@ public class View extends JFrame{
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		getContentPane().add(mainPanel);
 		
-		JPanel totalPanel = new JPanel(new BorderLayout());
+		JPanel totalPanel = new JPanel(new GridLayout(3,1));
 		mainPanel.add(totalPanel,BorderLayout.SOUTH);
 		
-		JPanel busInfoPanel = new JPanel(new BorderLayout());
+		JPanel busInfoPanel = new JPanel(new GridBagLayout());
 		mainPanel.add(busInfoPanel,BorderLayout.NORTH);
 		
 		JPanel routeInfoPanel = new JPanel(new BorderLayout());
@@ -48,24 +50,71 @@ public class View extends JFrame{
 		
 		
 		//working on totalPanel
-		total = new JLabel("Total");
-		totalPanel.add(total,BorderLayout.WEST);
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridx=0;
 		
+		BussesDeployed = new JLabel("Total Busses Deployed");
+		totalPanel.add(BussesDeployed);
+		c.gridx++;
 		
-		JPanel totalInfoPanel = new JPanel(new BorderLayout());
-		totalPanel.add(totalInfoPanel,BorderLayout.EAST);
+		Cost = new JLabel("Total Cost");
+		totalPanel.add(Cost);
+		c.gridx++;
 		
-		BussesDeployed = new JLabel("Busses Deployed");
-		totalInfoPanel.add(BussesDeployed,BorderLayout.WEST);
-		
-		Cost = new JLabel("Cost");
-		totalInfoPanel.add(Cost,BorderLayout.CENTER);
-		
-		CommuteTime = new JLabel("Commute Time");
-		totalInfoPanel.add(CommuteTime,BorderLayout.EAST);
-		
+		CommuteTime = new JLabel("Total Commute Time");
+		totalPanel.add(CommuteTime);
+		c.gridx=0;
 		
 		//working on busInfoPanel
+		BusCostLabel = new JLabel("Bus Cost");
+		busInfoPanel.add(BusCostLabel);
+		
+		BusCostEntry = new JTextField(7);
+		busInfoPanel.add(BusCostEntry);
+		
+		BusCapLabel = new JLabel("Bus Capacity");
+		busInfoPanel.add(BusCapLabel);
+	
+		BusCapEntry = new JTextField(7);
+		busInfoPanel.add(BusCapEntry);
+		
+		submit = new JButton("Submit");
+		busInfoPanel.add(submit);
+		
+		
+		//working on route info panel
+	
+		
+		Info = new JTextArea();
+		Info.setEditable(false);
+		Info.setLineWrap(true);
+		Info.setWrapStyleWord(true);
+		JScrollPane scroll= new JScrollPane(Info);
+		routeInfoPanel.add(Info,BorderLayout.CENTER);
+		routeInfoPanel.add(scroll);
+
+		
+		JPanel routeSelect = new JPanel(new GridLayout(1,2));
+		routeInfoPanel.add(routeSelect,BorderLayout.NORTH);
+		
+		route = new JComboBox();
+		routeSelect.add(route);
+		
+		JPanel bussesPanel = new JPanel();
+		routeSelect.add(bussesPanel);
+		Busses = new JLabel("Busses");
+		bussesPanel.add(Busses);
+		BusNum = new JTextField(5);
+		bussesPanel.add(BusNum);
+		
+		
+		
+		
+		
+	
+		
+		/*
 		JPanel busCost = new JPanel(new BorderLayout());
 		busInfoPanel.add(busCost,BorderLayout.WEST);
 		
@@ -85,7 +134,7 @@ public class View extends JFrame{
 		submit = new JButton("Submit");
 		busInfoPanel.add(submit, BorderLayout.EAST);
 		
-		
+		*/
 		
 		
 		
