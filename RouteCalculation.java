2227265200131e256f88d00a199692636189f0fd
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-11-03 11:30:03 -0600

    route calculation in works

--- RouteCalculation.java
+++ RouteCalculation.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 
 public class RouteCalculation{
-	public ArrayList<BusStop> busstops;
-	
+	private ArrayList<BusStop> busstops;
+	static int routeID = 0;
+	//size of bus = 50
+	//cost of the bus = 100
 	public RouteCalculation(ArrayList<BusStop> busstops)
 	{
 		this.busstops = busstops;
@@ -11,6 +13,15 @@ public class RouteCalculation{
 	public void calculateRoute()
 	{
 		printBusStops();
+		ArrayList<Route> routes = new ArrayList<Route>();
+		int size = busstops.size();
+		
+		if (size < 4) {
+			for (int i = 0; i < size; i++) {
+				int numbusses = Math.celi(busstops.get(i).getNumber_of_people() / 50.0);
+				routes.add(new Route());
+			}
+		}
 	}
 	
 	private void printBusStops()
@@ -18,7 +29,7 @@ public class RouteCalculation{
 		for (int i = 0; i < busstops.size(); i++) {
 			System.out.println(busstops.get(i).getStop().i + "," + busstops.get(i).getStop().j);
 			System.out.println("Number of people: " + busstops.get(i).getNumber_of_people());
-			System.out.println(busstops.get(i).getDestination()().i + "," + busstops.get(i).getDestination().j);
+			System.out.println(busstops.get(i).getDestination().i + "," + busstops.get(i).getDestination().j);
 		}
 	}
 }
