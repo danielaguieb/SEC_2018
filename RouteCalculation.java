061d6c6edbdde37c519b301e42233e8eb135e160
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-11-03 11:18:48 -0600

    pushing alot of changes to new file

--- RouteCalculation.java
+++ RouteCalculation.java
@@ -3,4 +3,22 @@ import java.util.ArrayList;
 public class RouteCalculation{
 	public ArrayList<BusStop> busstops;
 	
+	public RouteCalculation(ArrayList<BusStop> busstops)
+	{
+		this.busstops = busstops;
+	}
+	
+	public void calculateRoute()
+	{
+		printBusStops();
+	}
+	
+	private void printBusStops()
+	{
+		for (int i = 0; i < busstops.size(); i++) {
+			System.out.println(busstops.get(i).getStop().i + "," + busstops.get(i).getStop().j);
+			System.out.println("Number of people: " + busstops.get(i).getNumber_of_people());
+			System.out.println(busstops.get(i).getDestination()().i + "," + busstops.get(i).getDestination().j);
+		}
+	}
 }
